2ae9fe6d5cf11f97e6a1e09e9a1d7d233187b7da
foap <foap@BI101-13.epsevg.upc.es>
2019-04-23 10:54:09 +0200

    cambio2

---
+++ comparar-fechas.php
@@ -0,0 +1,20 @@
+<?php
+
+$fecha1="01-01-2015";
+$fecha2="01-02-2015";
+
+$fecha1=date("j/n/Y",strtotime("5 april 2001"));
+print("$fecha1");
+
+$fecha2=date("j/n/Y",strtotime("01-02-2015"));
+print("$fecha2");
+
+
+
+/*
+Resultado:
+5/4/2001
+*/
+
+
+?>
